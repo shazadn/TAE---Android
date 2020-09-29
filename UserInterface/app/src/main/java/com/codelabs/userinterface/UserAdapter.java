e7f39d05a819e99aaf8a535b46a0174d4fb4f0be
Shazad Nasim <s.nasim@hotmail.co.ukcd />
2020-09-29 13:08:53 +0100

    Changed styling card view elements

--- UserInterface/app/src/main/java/com/codelabs/userinterface/UserAdapter.java
+++ UserInterface/app/src/main/java/com/codelabs/userinterface/UserAdapter.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.codelabs.userinterface.entities.User;
@@ -17,6 +21,7 @@ import java.util.List;
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> {
     Context context;
     List<User> userList;
+    private int lastPosition = -1;
 
     public UserAdapter(Context context) {
         this.context = context;
@@ -25,6 +30,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> 
     public void setUserList(List<User> list) {
         userList = list;
         notifyDataSetChanged();
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_out_right);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
     }
 
     @NonNull
@@ -40,6 +53,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> 
         holder.tvEmail.setText(userList.get(position).getEmail());
         holder.tvCity.setText(userList.get(position).getCity());
         holder.tvBday.setText(userList.get(position).getBirthday());
+        setAnimation(holder.parent, position);
     }
 
     @Override
@@ -52,9 +66,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.MyViewHolder> 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView tvName, tvEmail, tvCity, tvBday;
+        CardView parent;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            parent = itemView.findViewById(R.id.parent);
+
             tvName = itemView.findViewById(R.id.tvName);
             tvEmail = itemView.findViewById(R.id.tvEmail);
             tvCity = itemView.findViewById(R.id.tvCity);
